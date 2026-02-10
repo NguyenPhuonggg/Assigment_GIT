c3d96de9256625637afcd087e6284a58efbbaeb4
NguyenPhuonggg <phuong2422004@gmail.com>
2026-02-10 22:57:29 +0700

    Fibonacci(flag=True)

--- Phuong/module1/Fibonaci.java
+++ Phuong/module1/Fibonaci.java
@@ -1,8 +1,11 @@
-public class Fibonaci{
-    public static int fibonacciRecursion(int n) {
-    if (n <= 1) {
-        return n;
+public class Fibonaci {
+    public int calculate(int n, boolean flag) {
+        if (flag) {
+            // Logic Đệ quy (Từ branch impl1)
+            if (n <= 1) {
+                return n;
+            }
+            return calculate(n - 1, true) + calculate(n - 2, true);
+        } 
     }
-    return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
- }
 }
