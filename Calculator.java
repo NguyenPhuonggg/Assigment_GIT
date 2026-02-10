09bdfbc16e823e89c8a39055f44a9b57e3d1963f
NguyenPhuonggg <phuong2422004@gmail.com>
2026-02-10 22:17:07 +0700

    Phep Tru

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,15 @@
 public class Calculator {
-    switch (sign) {
-        case "PLUS":
-            return arg1 + arg2;
-            default: break;
+
+    public double calculate(String sign, double arg1, double arg2) {
+        switch (sign.toUpperCase()) {
+            case "PLUS":
+                return arg1 + arg2;
+                
+            case "MINUS":
+                return arg1 - arg2;      
+            default:
+                System.out.println("Toán tử không hợp lệ: " + sign);
+                return 0;
         }
-    return 0;
+    }
 }
