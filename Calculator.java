dce71d1b4563915cdbdcc314aa717afbfc3fc62c
NguyenPhuonggg <phuong2422004@gmail.com>
2026-02-10 22:30:30 +0700

    Phep Chia

--- Calculator.java
+++ Calculator.java
@@ -8,7 +8,14 @@ public class Calculator {
             case "MINUS":
                 return arg1 - arg2;
             case "MULTIPLY":
-                return arg1 * arg2;   
+                return arg1 * arg2;  
+            case "DIVIDE":
+                if (arg2 == 0) {
+                    System.out.println("Lỗi: Chia cho số không.");
+                    return 0;
+                }else{
+                    return arg1 / arg2;
+                } 
             default:
                 System.out.println("Toán tử không hợp lệ: " + sign);
                 return 0;
