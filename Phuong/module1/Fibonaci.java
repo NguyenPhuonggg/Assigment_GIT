33c61b6072c965596a48c310850073d2459de938
NguyenPhuonggg <phuong2422004@gmail.com>
2026-02-10 22:58:38 +0700

    Fibonacci (flag=False)

--- Phuong/module1/Fibonaci.java
+++ Phuong/module1/Fibonaci.java
@@ -6,6 +6,21 @@ public class Fibonaci {
                 return n;
             }
             return calculate(n - 1, true) + calculate(n - 2, true);
-        } 
+        } else {
+            // Logic Vòng lặp (Từ branch impl2)
+            if (n <= 1) {
+                return n;
+            }
+            int t1 = 0;
+            int t2 = 1;
+            int sum = 0;
+            
+            for (int i = 2; i <= n; i++) {
+                sum = t1 + t2;
+                t1 = t2;
+                t2 = sum;
+            }
+            return t2;
+        }
     }
 }
